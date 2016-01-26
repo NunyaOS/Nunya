88e74de04c912375f372dfbb7d6b36ade0ce72d4
Alex Wu <alexmrwu@gmail.com>
2016-01-26 15:20:15 -0500

    Add Dox comment

--- src/graphics.h
+++ src/graphics.h
@@ -18,6 +18,19 @@ struct graphics_color {
 int graphics_width();
 int graphics_height();
 
+/** 
+ * @brief Draws a line on the display from (x1, y1) to (x2, y2)
+ * 
+ * @details Order of points does not matter. Points outside of display will be rounded to the closest edge
+ * 
+ * @param x1
+ * @param y1
+ * @param x2
+ * @param y2
+ * @param c: color of line
+ * 
+ * @return
+ */
 void graphics_line( int x1, int y1, int x2, int y2, struct graphics_color c );
 void graphics_rect( int x, int y, int w, int h, struct graphics_color c );
 void graphics_clear( struct graphics_color c );
