9a78e99f45e8640cd319ecb80a8aff6927b4f238
Alex Wu <alexmrwu@gmail.com>
2016-01-29 11:04:24 -0500

    Add deploy script; add encrypted keys for github
    Test deploy in the script
    
    Go back to parent directory
    
    Add mdkir check; quiet output for apt-get update
    
    Stop error on rm'ing empty directory
    
    Add deploy csript

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Based on this gist: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+set -e  # exit with nonzero exit code if anything fails
+
+# delete .iso in out directory
+
+mkdir -p out
+rm -f out/*
+
+# make the .iso
+cd src && make
+cd .. && cp src/*.iso out
+
+# create new Git repo in out/
+cd out
+git init
+git config user.name "Travis CI"
+git config user.email "alexmrwu@gmail.com"
+
+# Only commit to the new Git repo
+git add .
+git commit -m "Deploy .iso"
+
+# Force push from the current repo's master branch to the remote repo's build branch.
+# (All previous history will be lost since we are overwriting it)
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:build > /dev/null 2>&1
