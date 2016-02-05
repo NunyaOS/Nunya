05270a29f1040f31ab6bd6e02fbb8afdfe9fe96b
Shuyang Li <shuyang.li.95@gmail.com>
2016-02-05 15:12:19 -0500

    Revert "Separate memory and kmalloc modules"

--- src/main.c
+++ src/main.c
@@ -6,7 +6,6 @@ See the file LICENSE for details.
 
 #include "console.h"
 #include "memory.h"
-#include "memory_raw.h"     // memory_init
 #include "process.h"
 #include "interrupt.h"
 #include "keyboard.h"
