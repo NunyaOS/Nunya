5f193c1dd52f3d87676d4991766c5877fa43afef
Ryan <rboccabe@nd.edu>
2016-02-05 13:40:31 -0500

    DFS on ISO FS
    Runs independently from the kernel and runs DFS on a .iso file given as the lone command line argument, nicely printing out names of files and directories as it goes.

--- etc/iso.c
+++ etc/iso.c
@@ -1,14 +1,12 @@
 /*
- * iso.c
- *
- *  Created on: Jan 29, 2016
- *      Author: jesse
- */
+Copyright (C) 2015 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
 
-#include <stdio.h>
+#include "iso.h"
 #include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
+#include "kerneltypes.h"
 
 #define PVD_OFFSET 156
 #define ISO_BLOCK 2048
@@ -27,18 +25,16 @@ struct directory_record {
 	char file_identifier[2048];
 };
 
+void print_n_tabs(int n);
 void print_dr_name(struct directory_record *DR);
-void print_iso_date(char *src);
 int hex_to_int(char *src, int len);
-void print_file_system(FILE *fp);
 struct directory_record get_directory_record(FILE *fp);
-void print_directory_record(struct directory_record *DR);
 int is_dir(int flags);
 int is_valid_record(struct directory_record *DR);
-void print_subdirectory(FILE *fp, char *loc_of_subdir, char *loc_of_parent);
+void print_subdirectory(FILE *fp, char *loc_of_subdir, char *loc_of_parent, int depth);
 int have_equal_extents(char *first, char *second);
 
-void print_file_system(FILE *fp){
+void print_file_system(FILE *fp) {
 	fseek(fp, (16 * ISO_BLOCK) + PVD_OFFSET, SEEK_SET);
 	struct directory_record DR;
 	char loc_of_parent[8];
@@ -48,154 +44,126 @@ void print_file_system(FILE *fp){
 		printf("\n");
 	}
 	memcpy(loc_of_parent, DR.loc_of_ext, 8);
-	print_subdirectory(fp, DR.loc_of_ext, loc_of_parent);
+	print_subdirectory(fp, DR.loc_of_ext, loc_of_parent, 1);
 }
 
-void print_subdirectory(FILE *fp, char *loc_of_subdir, char *loc_of_parent){
+void print_subdirectory(FILE *fp, char *loc_of_subdir, char *loc_of_parent, int depth) {
 	fseek(fp, ISO_BLOCK * hex_to_int(loc_of_subdir + 4, 4), SEEK_SET);	// Go to extent of subdir
 	struct directory_record DR;
 	DR = get_directory_record(fp);
-	while(is_valid_record(&DR)){
+	while (is_valid_record(&DR)) {
 		if (is_dir(DR.file_flags[0])) {	// If it's a directory
-			if (have_equal_extents(DR.loc_of_ext, loc_of_parent)){	// Doesn't point to parent directory
+			if (have_equal_extents(DR.loc_of_ext, loc_of_parent)) {	// Doesn't point to parent directory
 				DR = get_directory_record(fp);	// Get the next DR in this extent
 				continue;
 			}
-			else if (have_equal_extents(DR.loc_of_ext, loc_of_subdir)) {	// Doesn't point to self
-				DR = get_directory_record(fp);	// Get the next DR in this extent
+			else if (have_equal_extents(DR.loc_of_ext, loc_of_subdir)) { // Doesn't point to self
+				DR = get_directory_record(fp); // Get the next DR in this extent
 				continue;
 			}
 			else {
+                print_n_tabs(depth);
 				print_dr_name(&DR);
 				int i_cur_pos = fseek(fp, 0, SEEK_CUR);
 				i_cur_pos = ftell(fp);
-				print_subdirectory(fp, DR.loc_of_ext, loc_of_subdir);
+				print_subdirectory(fp, DR.loc_of_ext, loc_of_subdir, depth+1);
 				fseek(fp, i_cur_pos, SEEK_SET);
 				DR = get_directory_record(fp);
 				continue;
 			}
 		}
 		else {
+            print_n_tabs(depth);
 			print_dr_name(&DR);
 			DR = get_directory_record(fp);
 		}
-
 	}
 	printf("\n");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s iso-file-name\n", argv[0]);
+        return 1;
+    }
+    
 	char fblock[2048];
-	char *fname = "/home/jesse/Documents/Open Source Software Development/test_ISOs/filesystem4.iso";
 
 	FILE *fp;
-	fp = fopen(fname,"r");
+	fp = fopen(argv[1],"r");
 	if (!fp) {
 		printf("Failed to open file\n");
 		return -1;
 	}
-
 	print_file_system(fp);
 }
 
-
-int hex_to_int(char *src, int len){
+int hex_to_int(char *src, int len) {
 	int i, sum = 0;
-	for(i = 0; i < len; i++){
+	for (i = 0; i < len; i++) {
 		sum *= 16;
 		sum += src[i];
 	}
 	return sum;
 }
-// Print ISO Date in format h:m:s m/d/y
-void print_iso_date(char *src){
-	printf("%d:%d:%d %d/%d/%d\n", src[3], src[4], src[5], src[1], src[2], src[0] + 1900);
-}
 
-int is_valid_record(struct directory_record *DR){
-	if(DR->rec_date_time[3] < 0 || DR->rec_date_time[3] >= 24){	// validate hours
+int is_valid_record(struct directory_record *DR) {
+	if(DR->rec_date_time[3] < 0 || DR->rec_date_time[3] >= 24) {	// validate hours
 		return 0;
 	}
-	if(DR->rec_date_time[4] < 0 || DR->rec_date_time[4] >= 60){	// validate minutes
+	if(DR->rec_date_time[4] < 0 || DR->rec_date_time[4] >= 60) {	// validate minutes
 		return 0;
 	}
-	if(DR->rec_date_time[5] < 0 || DR->rec_date_time[5] >= 60){	// validate seconds
+	if(DR->rec_date_time[5] < 0 || DR->rec_date_time[5] >= 60) {	// validate seconds
 		return 0;
 	}
-	if(DR->rec_date_time[1] <= 0 || DR->rec_date_time[1] > 12){	// validate month
+	if(DR->rec_date_time[1] <= 0 || DR->rec_date_time[1] > 12) {	// validate month
 		return 0;
 	}
-	if(DR->rec_date_time[2] <= 0 || DR->rec_date_time[2] > 31){	// validate day
+	if(DR->rec_date_time[2] <= 0 || DR->rec_date_time[2] > 31) {	// validate day
 		return 0;
 	}
-	if(DR->rec_date_time[0] < 0){	// validate year
+	if(DR->rec_date_time[0] < 0) {	// validate year
 		return 0;
 	}
 	return 1;
 }
 
-struct directory_record get_directory_record(FILE *fp){
+struct directory_record get_directory_record(FILE *fp) {
 	int bytes_read = 0;
 
 	struct directory_record DR;
-	fread(&(DR.length_of_record), sizeof(DR.length_of_record), 1, fp);
-	uint8_t length_of_dir_record = (DR.length_of_record);
-	fread(&(DR.length_of_ext_record), 1, sizeof(DR.length_of_ext_record), fp);
+	fread(&(DR.length_of_record), sizeof(DR.length_of_record), 1, fp);   //1 for bytes read
+	fread(&(DR.length_of_ext_record), 1, sizeof(DR.length_of_ext_record), fp);   //1 for bytes read
 	fread(DR.loc_of_ext, 1, 8, fp);
 	fread(DR.data_length, 1, 8, fp);
 	fread(DR.rec_date_time, 1, 7, fp);
 	fread(DR.file_flags, 1, 1, fp);
-	int flags = (int) (DR.file_flags[0]);
 	fread(DR.file_flags_interleaved, 1, 1, fp);
 	fread(&(DR.interleave_gap_size), 1, sizeof(DR.interleave_gap_size), fp);
 	fread(DR.vol_seq_num, 1, 4, fp);
 	fread(&(DR.len_identifier), 1, sizeof(DR.len_identifier), fp);
 	bytes_read += 33;
+
 	fread(DR.file_identifier, 1, DR.len_identifier, fp);
-	bytes_read += (DR.len_identifier);
-	fseek(fp, length_of_dir_record - bytes_read, SEEK_CUR);
+	DR.file_identifier[DR.len_identifier] = '\0';
+	bytes_read += DR.len_identifier;
+
+	fseek(fp, DR.length_of_record - bytes_read, SEEK_CUR);
 	return DR;
 }
 
-void print_dr_name(struct directory_record *DR){
-	DR->file_identifier[(DR->len_identifier)] = '\0';
-
+void print_dr_name(struct directory_record *DR) {
 	if (DR->file_identifier[0] > 0x20) {
-		printf("File Identifier: %s\n", DR->file_identifier);
+		printf("File/Dir Identifier: %s\n", DR->file_identifier);
 	}
-
 	else {
-		printf("File Identifier: /\n");
+		printf("File/Dir Identifier: /\n");
 	}
 }
 
-void print_directory_record(struct directory_record *DR){
-	uint8_t length_of_dir_record = DR->length_of_record;
-	printf("Length of Record: %d\n", length_of_dir_record);
-	printf("length of Extended Record: %d\n", DR->length_of_ext_record);
-	printf("Location of Extent (LE): %x..%x..%x..%x\n", DR->loc_of_ext[0], DR->loc_of_ext[1], DR->loc_of_ext[2], DR->loc_of_ext[3]);
-	printf("Location of Extent (BE): %x..%x..%x..%x\n", DR->loc_of_ext[4], DR->loc_of_ext[5], DR->loc_of_ext[6], DR->loc_of_ext[7]);
-	printf("Offset to extent: %d\n", hex_to_int(DR->loc_of_ext+4, 4));
-	printf("Length of Data (LE): %x..%x..%x..%x\n", DR->data_length[0], DR->data_length[1], DR->data_length[2], DR->data_length[3]);
-	printf("Length of Data (BE): %x..%x..%x..%x\n", DR->data_length[4], DR->data_length[5], DR->data_length[6], DR->data_length[7]);
-	print_iso_date(DR->rec_date_time);
-	printf("Interleaved Gap Size: %d\n", DR->interleave_gap_size);
-	printf("Volume Sequence Number (LE): %x..%x\n", DR->vol_seq_num[0], DR->vol_seq_num[1]);
-	printf("Volume Sequence Number (BE): %x..%x\n", DR->vol_seq_num[2], DR->vol_seq_num[3]);
-	printf("Length of Identifier: %d\n", DR->len_identifier);
-	DR->file_identifier[(DR->len_identifier)] = '\0';
-
-	if (DR->file_identifier[0] > 0x20) {
-		printf("File Identifier: %s\n", DR->file_identifier);
-	}
-
-	else {
-		printf("/\n");
-	}
-}
-
-int is_dir(int flags){
-	if((flags >> 1) & 1) {
+int is_dir(int flags) {
+	if ((flags >> 1) & 1) {
 		return 1;
 	}
 	else {
@@ -203,11 +171,18 @@ int is_dir(int flags){
 	}
 }
 
-int have_equal_extents(char *first_ext, char *second_ext){
+int have_equal_extents(char *first_ext, char *second_ext) {
 	int ext1 = hex_to_int((first_ext) + 4, 4);
 	int ext2 = hex_to_int((second_ext) + 4, 4);
-	if(ext1 == ext2) {
+	if (ext1 == ext2) {
 		return 1;
 	}
 	return 0;
+}
+
+void print_n_tabs(int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("\t");
+    }
 }
