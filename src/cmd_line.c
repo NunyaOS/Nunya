37ec3d7b02ddcd9184fce3e12c40bf087ad41755
jhamilt5 <jessehamilton4@gmail.com>
2016-02-25 15:39:16 -0500

    Handle cd Call Piece-wise (and documentation)
    Handle cd call one piece at a time, separated by '/'. Allows for full
    relative path arguments such as ../HOME or ../..
    Tested by moving through filesystem with cd calls.

--- src/cmd_line.c
+++ src/cmd_line.c
@@ -11,15 +11,35 @@ See the file LICENSE for details.
 #include "iso.h"
 
 #define KEYBOARD_BUFFER_SIZE 256
+#define ROOT "/"
 
 char cur_path[256];
 
 void move_up_directory();
+void move_into_directory(const char *dname);
+void try_cd(const char *dpath, const char *dname);
+void process_cd_part(const char *dname);
+void set_cur_path(const char *dpath);
 
+/**
+ * @brief Initialize the command line
+ */
 void cmd_line_init() {
-    strcpy(cur_path, "/");
+    set_cur_path(ROOT);
 }
 
+/**
+ * @brief Set the current working directory
+ * @param dpath The path to which to set the current working directory
+ */
+void set_cur_path(const char *dpath) {
+    strcpy(cur_path, dpath);
+}
+
+/**
+ * @brief Moves up one directory
+ * @details Changes working directory to the parent of current directory
+ */
 void move_up_directory() {
     if (strcmp(cur_path, "/") == 0) {
         console_printf("cd: root has no parent directory\n");
@@ -29,7 +49,7 @@ void move_up_directory() {
     // Move up one directory by finding the last occurrence of '/'
     for (i = strlen(cur_path); i >= 0; i--) {
         if (i == 0) {
-            strcpy(cur_path, "/");
+            set_cur_path(ROOT);
             return;
         }
         else if (cur_path[i] == '/') {
@@ -39,47 +59,78 @@ void move_up_directory() {
     }
 }
 
+/**
+ * @brief Move into a directory below yourself
+ * @details Move into the directory below yourself specified by dname
+ * @param dname Name of directory into which you attempt to move
+ */
+void move_into_directory(const char *dname) {
+    char dpath[256];
+    strcpy(dpath, cur_path);
+    if (strcmp(cur_path, ROOT) != 0) {   // Not at root, so we need to add a '/' to the path
+        memcpy(dpath + strlen(cur_path), "/", 1);
+        memcpy(dpath + strlen(cur_path) + 1, dname, strlen(dname) + 1);
+    } else {
+        memcpy(dpath + strlen(cur_path), dname, strlen(dname) + 1);
+    }
+    try_cd(dpath, dname);
+}
+
+/**
+ * @brief Moves up or down one directory
+ * @details Helper function of cmd_line_cd
+ * Moves up or down one directory, specified by dname
+ * @param dname Next directory in which to move
+ */
+void process_cd_part(const char *dname) {
+    if (strcmp(dname, "..") == 0) {     // Move up one directory
+        move_up_directory();
+        return;
+    } else {
+        move_into_directory(dname);
+        return;
+    }
+}
+
+/**
+ * @brief Try to move into a new directory
+ * @details Try to move into the directory at dpath.
+ * If not possible, doesn't update cur_path.
+ * @param dpath Full path to new directory location
+ * @param dname Name of new directory location
+ */
+void try_cd(const char *dpath, const char *dname) {
+    struct iso_dir *dir = iso_dopen(dpath, 3);
+    if (!dir) {
+        console_printf("cd: %s is not a directory\n", dname);
+        return;
+    }
+    iso_dclose(dir);
+    set_cur_path(dpath);
+}
+
+/**
+ * @brief Change current working directory
+ * @details Change current working directory to that specified by arg_line
+ * @param arg_line All arguments given after command 'cd'
+ */
 void cmd_line_cd(const char *arg_line) {
     char line_copy[strlen(arg_line) + 1];
     strcpy(line_copy, arg_line);
     char *first_word = strtok(line_copy, " ");
 
-    // If you give no arguments to cd, you are sent to root
-    if (strlen(first_word) == 0) {
-        strcpy(cur_path, "/");
+    // If you give no arguments to cd or the argument is "/", you are sent to root
+    if (strlen(first_word) == 0 || strcmp(first_word, ROOT) == 0) {
+        set_cur_path(ROOT);
         return;
     }
 
-    struct iso_dir *dir;
-    char dir_arg[256];
-    if (strcmp(first_word, ".") == 0) {
-        // Do nothing, just going to self directory
-        return;
-    } else if (strcmp(first_word, "..") == 0) {     // Move up a directory
-        move_up_directory();
-        return;
-    } else if (first_word[0] == '/') {   // Argument is an absolute path
-        strcpy(dir_arg, first_word);
-    } else {    // Argument is a relative path
-        if (first_word[0] == '.' && first_word[1] == '/') {
-            first_word = first_word + 2;    // Forget about the ./ and move it along
-        }
-        strcpy(dir_arg, cur_path);
-        if (strcmp(cur_path, "/") != 0) {
-            memcpy(dir_arg + strlen(cur_path), "/", 1);
-            memcpy(dir_arg + strlen(cur_path) + 1, first_word, strlen(first_word) + 1);
-        } else {
-            memcpy(dir_arg + strlen(cur_path), first_word, strlen(first_word) + 1);
-        }
+    // Process path piece-wise separated by '/'
+    char *next_dir = strtok(first_word, "/");
+    while (next_dir) {
+        process_cd_part(next_dir);
+        next_dir = strtok(0, "/");
     }
-
-    dir = iso_dopen(dir_arg, 3);
-    if (!dir) {
-        console_printf("cd: %s is not a directory\n", first_word);
-        return;
-    }
-    iso_dclose(dir);
-    strcpy(cur_path, dir_arg);
 }
 
 // Currently does not use arg_line at all
