adf0e41bfd83425e1f6cab4a063d6a1eb942671e
Ryan <rboccabe@nd.edu>
2016-04-07 05:41:04 -0400

    Includes GPL and Copyright in all files

--- src/fs_sys.c
+++ src/fs_sys.c
@@ -1,3 +1,9 @@
+/*
+Copyright (C) 2015 The University of Notre Dame
+This software is distributed under the GNU General Public License.
+See the file LICENSE for details.
+*/
+
 #include "fs_sys.h"
 #include "string.h"
 #include "kerneltypes.h"
