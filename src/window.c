5d6e2e64173155685a632621fd72828db4e92a20
Alex Wu <alexmrwu@gmail.com>
2016-04-28 21:53:38 -0400

    Replace begin_draw with init

--- src/window.c
+++ src/window.c
@@ -11,6 +11,12 @@ See the file LICENSE for details.
 #define CHARACTER_W 8
 #define CHARACTER_H 8
 
+// a special number to see if the offset calculation has been done
+// TODO: change b/c we want to allow offsets to be negative (when moving window out of frame)
+#define OFFSET_INIT -1
+// this can be -1 as bounds will always be [0, graphics_width/height)
+#define BOUNDS_INIT -1
+
 static inline void window_clear_bounds(struct window *win) {
     win->bounds_x_1 = 0;
     win->bounds_x_2 = 0;
@@ -19,22 +25,23 @@ static inline void window_clear_bounds(struct window *win) {
 }
 #include "console.h"
 static inline void window_set_bounds(struct window *win, int x, int y, int w, int h) {
-    // check if bounds_x_1 ... y_2 are not 0.
-    // if not, do calculation. otherwise, do nothing
+    // check if bounds_x_1 ... y_2 are -1.
+    // if so, do calculation. otherwise, do nothing
     win->bounds_x_1 = x < 0 ? 0 : x;
-    console_printf("bounds_x_2 %d\n", x + w);
     win->bounds_x_2 = x + w >= graphics_width() ? graphics_width() - 1 : x + w;
     win->bounds_y_1 = y < 0 ? 0 : y;
-    console_printf("bounds_y_2 %d\n", y + h);
     win->bounds_y_2 = y + h >= graphics_height() ? graphics_height() - 1 : y + h;
 }
 
 // This function prepares to draw in a window by setting up its boundary
 // restrictions.
-// THIS FUNCTION SHOULD ALWAYS BE CALLED BEFORE MAKING A GRAPHICS DRAW
-// CALL IN THE WINDOW SYSTEM
-static inline void window_begin_draw(struct window *w) {
-    // TODO: cache the x/y offset and the truncated height and width (done below)
+// THIS FUNCTION SHOULD ALWAYS BE CALLED WHEN MAKING A WINDOW
+// OR MOVING A WINDOW
+static inline void window_init_draw(struct window *w) {
+	// if one bounds number has been set, have already calculated all bounds and offsets
+	if (w->bounds_x_1 != BOUNDS_INIT && w->x_offset != OFFSET_INIT) {
+		return;
+	}
     int x = w->x;
     int y = w->y;
     struct window *init = w;
@@ -56,17 +63,9 @@ static inline void window_begin_draw(struct window *w) {
 
     init->x_offset = x;
     init->y_offset = y;
-
-    window_set_bounds(w, x, y, width, height);
-}
-
-// This function removes the restrictions used to enforce window clipping
-// on the graphics system
-// THIS FUNCTION SHOULD ALWAYS BE CALLED IF window_begin_draw WAS CALLED
-// TO FINISH THE DRAW
-static inline void window_end_draw(struct window *w) {
-    w->x_offset = 0;
-    w->y_offset = 0;
+	console_printf("x_offset %d\n", x);
+	console_printf("y_offset %d\n", y);
+    window_set_bounds(init, x, y, width, height);
 }
 
 struct window *window_create(int x, int y, int width, int height, struct window *parent) {
@@ -79,15 +78,19 @@ struct window *window_create(int x, int y, int width, int height, struct window 
     w->parent = parent;
     struct list l = {0, 0};
     w->event_queue = l;
-    w->x_offset = 0;
-    w->y_offset = 0;
+    w->x_offset = OFFSET_INIT;
+    w->y_offset = OFFSET_INIT;
+    w->bounds_x_1 = BOUNDS_INIT;
+    w->bounds_x_2 = BOUNDS_INIT;
+    w->bounds_y_1 = BOUNDS_INIT;
+    w->bounds_y_2 = BOUNDS_INIT;
+	window_init_draw(w);
 
     // Draw the border
     struct graphics_color bc = {128,128,128};
     window_set_border_color(w, bc);
 
     active_window = w;
-
     return w;
 }
 
@@ -102,40 +105,32 @@ void window_set_border_color(struct window *w, struct graphics_color border_colo
 }
 
 void window_draw_line(struct window *w, int x1, int y1, int x2, int y2, struct graphics_color color) {
-    window_begin_draw(w);
     x1 += w->x_offset;
     y1 += w->y_offset;
     x2 += w->x_offset;
     y2 += w->y_offset;
     graphics_line(x1, y1, x2, y2, color, 0);
-    window_end_draw(w);
 }
 
 void window_draw_arc(struct window *w, int x, int y, double r, double start_theta, double end_theta, struct graphics_color color) {
-    window_begin_draw(w);
     graphics_arc(x + w->x, y + w->y, r, start_theta, end_theta, color);
-    window_end_draw(w);
 }
 
 void window_draw_circle(struct window *w, int x, int y, double r, struct graphics_color color) {
-    window_begin_draw(w);
     graphics_circle(x + w->x, y + w->y, r, color);
-    window_end_draw(w);
 }
 
 void window_draw_char(struct window *w, int x, int y, char ch, struct graphics_color fgcolor,
                       struct graphics_color bgcolor) {
-    window_begin_draw(w);
     x += w->x_offset;
     y += w->y_offset;
     graphics_char(x, y, ch, fgcolor, bgcolor);
-    window_end_draw(w);
 }
 
 void window_draw_string(struct window *w, int x, int y, const char *str, struct graphics_color fgcolor,
                         struct graphics_color bgcolor) {
-    x += w->x_offset;
-    y += w->y_offset;
+    /*x += w->x_offset;*/
+    /*y += w->y_offset*/
     int pos_h = 0;
     int pos_w = 0;
     while (*str) {
@@ -154,10 +149,9 @@ void window_draw_string(struct window *w, int x, int y, const char *str, struct 
 void window_draw_bitmap(struct window *w, int x, int y, int width, int height, uint8_t * data,
                      struct graphics_color fgcolor,
                      struct graphics_color bgcolor) {
-    window_begin_draw(w);
     graphics_bitmap(x + w->x, y + w->y, width, height, data, fgcolor, bgcolor);
-    window_end_draw(w);
 }
+
 
 void window_hierarchy_test() {
     struct graphics_color text_color = {0,255,0};
@@ -166,7 +160,7 @@ void window_hierarchy_test() {
     struct window *top = window_create(30, 30, 500, 400, 0);
     struct window *child = window_create(100, 200, 500, 300, top);
     window_draw_string(child, 250, 0, "Hello world!", text_color, text_bg);
-
     kfree(top);
     kfree(child);
 }
+
